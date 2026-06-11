73a17a8ce33e7800814c26b651e544521898bb56
Praveen Dhankhar <praveendhankhar@Mac.lan>
2026-06-11 22:43:28 +0530

    Advisor.java modified

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Advisor {
@@ -28,6 +32,9 @@ public class Advisor {
     @Column(nullable = false)
     private String email;
 
+    @OneToMany(mappedBy = "advisor")
+    private List<Client> clients;
+
     protected Advisor() {
 
     }
@@ -38,6 +45,7 @@ public class Advisor {
         this.address = address;
         this.phone = phone;
         this.email = email;
+        this.clients = new ArrayList<>();
     }
 
     public Long getAdvisorId() {
@@ -82,5 +90,13 @@ public class Advisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
     }
 }
